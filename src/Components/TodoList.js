38eac1df6c32ccd89931199c0a8d971fe641d8b4
sinankinav <78664500+sinankinav@users.noreply.github.com>
2021-05-27 16:04:26 +0300

    sd

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -8,7 +8,7 @@ export default class TodoList extends Component {
                 this.props.tasks.map(task=>{
                     return(
                         <div key={task.id}>
-                            <li className={task.done ? "done" :""} onClick={()=>this.props.cizTodo(task.name)}>{task.name}</li>
+                            <li className={task.done ? "done":""} onClick={()=>this.props.cizTodo(task.name)}>{task.name}</li>
                             <button onClick={()=>this.props.removeTodo(task.name)}>delete</button>
                         </div>
                     )
